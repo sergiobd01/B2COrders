3a3273007f605f1073098367d360ce7efb24d25f
Diana Martinez <dianal.martinezt@javeriana.edu.co>
2018-11-05 12:01:07 -0500

    modify pom

--- src/main/java/com/orderService/OrderServiceApplication.java
+++ src/main/java/com/orderService/OrderServiceApplication.java
@@ -1,9 +1,18 @@
 package com.orderService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(
+		basePackages = {
+				"com.orderService"
+		}
+		
+		)
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
